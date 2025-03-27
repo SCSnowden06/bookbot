44955be366dcdc15dc8b73704363c7a2012853e9
SCSnowden06 <SCSnowden06@gmail.com>
2025-03-27 18:07:50 -0400

    added mobydick and prideandprejudice as book options, also added user input NO VALIDATION using sys.argv

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from stats import get_book_text , get_num_words , get_num_char , get_char_count , order_char_count
+import sys 
+
+args = sys.argv
 
 def main():
-    filePath = "books/frankenstein.txt"
+    filePath = args[1]
     bookText = get_book_text(filePath)
     amtWords = get_num_words(bookText)
     charCount = get_char_count(bookText)
     orderedCount = order_char_count(charCount)
 
+    print(f"Usage: python3 main.py <{args[1]}>\n")
+
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at '{args[1]}'...")
     print("----------- Word Count ----------")
     print(f"Found {amtWords} total words")
     print("--------- Character Count -------")
